274933bfbbdc8d318ddbb6e7602bbc4fb285bbcc
charafmrah <charaf4charaf@gmail.com>
2022-12-07 00:35:11 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import "../styles/globals.css";
 
 export default function RootLayout({
@@ -11,10 +12,13 @@ export default function RootLayout({
       <head />
       <body>
         <main>
-          <nav className="flex gap-5 text-2xl font-semibold">
-            <Link href="/">Dashboard</Link>
-            <Link href="/account">Account</Link>
-          </nav>
+          <div className="flex items-center justify-between py-5 mb-10 ">
+            <Image src="/logo.svg" width={50} height={50} />
+            <nav className="flex gap-5 text-2xl font-semibold align-middle ">
+              <Link href="/">Dashboard</Link>
+              <Link href="/account">Account</Link>
+            </nav>
+          </div>
           {children}
         </main>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Home Monitor</h1>
     </div>
   );
 }
