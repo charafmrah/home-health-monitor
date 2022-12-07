e1711eec8823ed0ea00a79a4fc69ccb12ad34e8c
charafmrah <charaf4charaf@gmail.com>
2022-12-07 16:27:13 +0300

    update

--- components/TemperatureLineChart.js
+++ components/TemperatureLineChart.js
@@ -3,25 +3,31 @@
 import { Chart } from "react-google-charts";
 
 const data = [
-  ["Time", "Value"],
-  [12, 30],
-  [13, 22],
-  [14, 20],
+  ["Time", "Temperature"],
+  [new Date("Dec 5, 2022"), 20],
+  [new Date("Dec 6, 2022"), 22],
+  [new Date("Dec 7, 2022"), 21],
 ];
 
 const options = {
   backgroundColor: "transparent",
+
   colors: ["#e0440e", "#e6693e", "#ec8f6e", "#f3b49f", "#f6c7b6"],
 
   hAxis: {
     textStyle: { color: "#FFF" },
   },
   vAxis: {
+    title: "Temperature (C)",
+    titleTextStyle: { color: "#FFF" },
     textStyle: { color: "#FFF" },
+    minValue: -10,
   },
   legend: {
     textStyle: { color: "#FFF" },
+    position: "bottom",
   },
+  curveType: "function",
   is3D: true,
 };
 
