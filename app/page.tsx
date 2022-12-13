4a0ae78b5824156848bd9becc69166e0f1a0ed9a
charafmrah <charaf4charaf@gmail.com>
2022-12-13 19:17:57 +0300

    Amplify configure fixed

--- app/page.tsx
+++ app/page.tsx
@@ -7,35 +7,46 @@ import HumidityLineChart from "../components/HumidityLineChart";
 import PulseDot from "../components/PulseDot";
 import Switch from "@mui/material/Switch";
 import { Amplify, PubSub } from "aws-amplify";
-import { AWSIoTProvider } from "@aws-amplify/pubsub/lib/Providers";
+import { AWSIoTProvider } from "@aws-amplify/pubsub";
 
 export default function HomePage() {
   const [deviceStatus, setDeviceStatus] = useState("online");
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     const amplifyConfig = {
       Auth: {
-        identityPoolId: process.env.AWS_IDENTITY_POOL_ID,
-        region: process.env.AWS_REGION,
-        userPoolId: process.env.AWS_USER_POOL_ID,
-        userPoolWebClientId: process.env.AWS_USER_POOL_WEB_CLIENT_ID,
+        identityPoolId: process.env.NEXT_PUBLIC_AWS_IDENTITY_POOL_ID,
+        region: process.env.NEXT_PUBLIC_AWS_REGION,
+        userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID,
+        userPoolWebClientId:
+          process.env.NEXT_PUBLIC_AWS_USER_POOL_WEB_CLIENT_ID,
       },
     };
 
     const awsIotConfig = new AWSIoTProvider({
-      aws_pubsub_region: process.env.NEXT_REGION,
-      aws_pubsub_endpoint: `wss://${process.env.NEXT_MQTT_ID}.iot.${process.env.REACT_APP_REGION}.amazonaws.com/mqtt`,
+      aws_pubsub_region: process.env.NEXT_PUBLIC_NEXT_REGION,
+      aws_pubsub_endpoint: `wss://${process.env.NEXT_PUBLIC_NEXT_MQTT_ID}.iot.${process.env.REACT_APP_REGION}.amazonaws.com/mqtt`,
     });
 
     Amplify.configure(amplifyConfig);
     Amplify.addPluggable(awsIotConfig);
 
-    Amplify.PubSub.subscribe("esp8266/pub").subscribe({
-      next: (data: any) => console.log("Message received", data),
+    console.log("hello");
+    const pubsub = PubSub.subscribe("esp8266/pub").subscribe({
+      next: (data: any) => {
+        console.log("hello");
+        console.log("Message received", data);
+        setData(data.value);
+      },
       error: (error: any) => console.error(error),
-      close: () => console.log("Done"),
+      complete: () => console.log("Done"),
     });
-  }, []);
+
+    return function cleanup() {
+      pubsub.unsubscribe();
+    };
+  });
 
   return (
     <div className="flex flex-col items-center">
@@ -45,6 +56,7 @@ export default function HomePage() {
         <p>Device {deviceStatus}</p>
         <Switch defaultChecked color="primary" />
       </div>
+      {data}
       <TemperatureLineChart />
       <HumidityLineChart />
     </div>
