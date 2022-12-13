756b043ac8b8601b970a168088f19f823fc72e58
charafmrah <charaf4charaf@gmail.com>
2022-12-13 17:51:49 +0300

    AWS Amplify PubSub setup

--- app/page.tsx
+++ app/page.tsx
@@ -6,32 +6,36 @@ import TemperatureLineChart from "../components/TemperatureLineChart";
 import HumidityLineChart from "../components/HumidityLineChart";
 import PulseDot from "../components/PulseDot";
 import Switch from "@mui/material/Switch";
-
-import Amplify, { PubSub } from "aws-amplify";
+import { Amplify, PubSub } from "aws-amplify";
 import { AWSIoTProvider } from "@aws-amplify/pubsub/lib/Providers";
 
 export default function HomePage() {
   const [deviceStatus, setDeviceStatus] = useState("online");
 
-  Amplify.configure({
-    Auth: {
-      identityPoolId: process.env.AWS_IDENTITY_POOL_ID,
-      region: process.env.AWS_REGION,
-      userPoolId: process.env.AWS_USER_POOL_ID,
-      userPoolWebClientId: process.env.AWS_USER_POOL_WEB_CLIENT_ID,
-    },
-  });
-  Amplify.addPluggable(
-    new AWSIoTProvider({
-      aws_pubsub_region: process.env.REACT_APP_REGION,
-      aws_pubsub_endpoint: `wss://${process.env.REACT_APP_MQTT_ID}.iot.${process.env.REACT_APP_REGION}.amazonaws.com/mqtt`,
-    })
-  );
-  Amplify.PubSub.subscribe("esp8266/pub").subscribe({
-    next: (data) => console.log("Message received", data),
-    error: (error: any) => console.error(error),
-    close: () => console.log("Done"),
-  });
+  useEffect(() => {
+    const amplifyConfig = {
+      Auth: {
+        identityPoolId: process.env.AWS_IDENTITY_POOL_ID,
+        region: process.env.AWS_REGION,
+        userPoolId: process.env.AWS_USER_POOL_ID,
+        userPoolWebClientId: process.env.AWS_USER_POOL_WEB_CLIENT_ID,
+      },
+    };
+
+    const awsIotConfig = new AWSIoTProvider({
+      aws_pubsub_region: process.env.NEXT_REGION,
+      aws_pubsub_endpoint: `wss://${process.env.NEXT_MQTT_ID}.iot.${process.env.REACT_APP_REGION}.amazonaws.com/mqtt`,
+    });
+
+    Amplify.configure(amplifyConfig);
+    Amplify.addPluggable(awsIotConfig);
+
+    Amplify.PubSub.subscribe("esp8266/pub").subscribe({
+      next: (data: any) => console.log("Message received", data),
+      error: (error: any) => console.error(error),
+      close: () => console.log("Done"),
+    });
+  }, []);
 
   return (
     <div className="flex flex-col items-center">
@@ -39,7 +43,7 @@ export default function HomePage() {
       <div className="flex items-center gap-2 mt-9">
         <PulseDot color={deviceStatus} />
         <p>Device {deviceStatus}</p>
-        <Switch defaultChecked color="primary" onChange={deviceStatusChange} />
+        <Switch defaultChecked color="primary" />
       </div>
       <TemperatureLineChart />
       <HumidityLineChart />
